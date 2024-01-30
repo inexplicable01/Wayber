290fbe4937b3656443392f77642ff5dbbb16d780
Saurabh Singh <saurabh.singhdev15@gmail.com>
2024-01-30 13:04:36 +0530

    Added reducer file

---
+++ src/store/clientProfile/reducer.js
@@ -0,0 +1,51 @@
+import * as actionTypes from "./actionTypes";
+
+// Define the initial state of the form
+const initialState = {
+  firstName: "",
+  middleName: "",
+  lastName: "",
+  age: "",
+  email: "",
+  estimatedAnnualIncome: "",
+  currentAddress: "",
+  desiredLocation: "",
+  loading: false,
+  error: null,
+  success: null,
+};
+
+// The form reducer function
+const clientProfileReducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case actionTypes.SUBMIT_FORM:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+        success: null,
+      };
+    case actionTypes.SUBMIT_FORM_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        success: "Form submitted successfully!",
+      };
+    case actionTypes.SUBMIT_FORM_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+      case actionTypes.RESET_SUCCESS_STATE:
+      return {
+        ...state,
+        success: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default clientProfileReducer;
